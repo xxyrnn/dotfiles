ac096fc63a79c96cdaf6d209c36bd753c7526f83
Xyrn <xyrnproject@gmail.com>
2025-12-19 14:20:08 +0100

    edited none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,24 @@
 -- none-ls for simplified lsp usage
 -- https://github.com/nvimtools/none-ls.nvim
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local null_ls = require "null-ls"
-        local formatting = null_ls.builtins.formatting
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require "null-ls"
+    local formatting = null_ls.builtins.formatting
 
-        null_ls.setup {
-            debug = false,
-            sources = {
-                formatting.stylua,
-                formatting.prettier,
-                formatting.black,
-                null_ls.builtins.diagnostics.flake8,
-                null_ls.builtins.completion.spell,
-            },
-        }
-    end,
+    null_ls.setup {
+      debug = false,
+      sources = {
+        require "none-ls.diagnostics.flake8",
+        formatting.stylua,
+        formatting.prettier,
+        formatting.black,
+        null_ls.builtins.completion.spell,
+      },
+    }
+  end,
 }
