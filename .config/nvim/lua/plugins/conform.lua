f54705779bea96828dcbcbd71c26bb7b4cc3e13d
Xyrn <xyrnproject@gmail.com>
2025-12-19 13:45:02 +0100

    updated files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,14 +1,16 @@
 -- conform for formatting
 -- https://github.com/stevearc/conform.nvim
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            html = { "prettier" },
-            css = { "prettier" },
-        },
-        format_on_save = {},
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      html = { "prettier" },
+      css = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
     },
+    format_on_save = {},
+  },
 }
